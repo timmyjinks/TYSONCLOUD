8bf7216880fac62fdd986064f3e58ee97c711da1
timmyjinks <tysonjjenkins.github@gmail.com>
2025-09-29 10:22:14 -0600

    Add jwt security for the api

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -3,17 +3,22 @@ import type { PageServerLoad } from './$types';
 import { BACKEND_URL } from '$env/static/private';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	const session = await locals.supabase.auth.getUser();
-	if (!session.data.user) {
+	const user = await locals.supabase.auth.getUser();
+	const session = await locals.supabase.auth.getSession();
+	if (!user.data.user) {
 		redirect(302, '/login');
 	}
 
 	try {
-		const response = await fetch(`${BACKEND_URL}/deployments?username=${locals.user?.email}`);
+		const response = await fetch(`${BACKEND_URL}/deployments?username=${locals.user?.email}`, {
+			headers: {
+				Authorization: 'Bearer ' + session.data.session?.access_token
+			}
+		});
 		const data = await response.json();
 
 		return {
-			deployments: data
+			deployments: data.data
 		};
 	} catch (err) {
 		return { deployments: [] };
@@ -24,10 +29,10 @@ export const actions = {
 	create: async ({ request, locals }) => {
 		const formData = await request.formData();
 		const name = formData.get('name') as string;
-		const url = formData.get('url') as string;
+		const source = formData.get('url') as string;
 		const env_array = JSON.parse(formData.get('env')) ?? [];
 		const volume = (formData.get('volume') as string) || '/data';
-		console.log(volume);
+		const session = await locals.supabase.auth.getSession();
 
 		let env = {};
 		for (let i = 0; i < env_array.length; i++) {
@@ -43,10 +48,13 @@ export const actions = {
 			...options,
 			signal: controller.signal,
 			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + session.data.session?.access_token
+			},
 			body: JSON.stringify({
 				name: name,
-				image: url,
+				source: source,
 				username: locals.user.email,
 				env: env,
 				volume: volume
@@ -61,13 +69,14 @@ export const actions = {
 		}
 		redirect(302, '/dashboard');
 	},
-	update: async ({ request }) => {
+	update: async ({ request, locals }) => {
 		const formData = await request.formData();
 		const id = formData.get('id') as string;
 		const new_name = formData.get('name') as string;
 		const update = formData.get('update') ? true : false;
-		const volume = formData.get('volume') as string;
+		const volume = (formData.get('volume') as string) ?? '/data';
 		const env_array = JSON.parse(formData.get('env')) ?? [];
+		const session = await locals.supabase.auth.getSession();
 
 		let env = {};
 		for (let i = 0; i < env_array.length; i++) {
@@ -83,7 +92,10 @@ export const actions = {
 			...options,
 			signal: controller.signal,
 			method: 'PUT',
-			headers: { 'Content-Type': 'application/json' },
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + session.data.session?.access_token
+			},
 			body: JSON.stringify({
 				id: id,
 				new_name: new_name,
@@ -101,13 +113,17 @@ export const actions = {
 		}
 		redirect(302, '/dashboard');
 	},
-	delete: async ({ request }) => {
+	delete: async ({ request, locals }) => {
 		const formData = await request.formData();
 		const id = formData.get('id');
+		const session = await locals.supabase.auth.getSession();
 
 		const response = await fetch(`${BACKEND_URL}/delete`, {
 			method: 'DELETE',
-			headers: { 'Content-Type': 'application/json' },
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + session.data.session?.access_token
+			},
 			body: JSON.stringify({
 				id: id
 			})
