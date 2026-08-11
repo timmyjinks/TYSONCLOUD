f9f1f5e1866f02b294cc36439f3c80dc061fe985
timmyjinks <tysonjjenkins.github@gmail.com>
2026-08-10 22:30:30 -0600

    fix: revert to cluster config

--- backend/kubernetes/kubernetes.go
+++ backend/kubernetes/kubernetes.go
@@ -6,8 +6,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
-	// "k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/rest"
 	gatewayclient "sigs.k8s.io/gateway-api/pkg/client/clientset/versioned"
 )
 
@@ -19,14 +18,9 @@ type KubernetesService struct {
 }
 
 func NewKubernetesService(kubeconfigPath string) (*KubernetesService, error) {
-	// config, err := rest.InClusterConfig()
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	config, err := rest.InClusterConfig()
 	if err != nil {
-		return nil, err
+		panic(err.Error())
 	}
 
 	clientset := kubernetes.NewForConfigOrDie(config)
